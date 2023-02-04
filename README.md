d0c1181ddc457066a6b10bc922dc95e55d3a1892
Alex Shaw <alex.shaw.as@gmail.com>
2023-02-03 19:01:26 -0800

    Add instructions and inspector.

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ![Deploy to GitHub Pages](https://github.com/Symbitic/BabylonJS-Doom-Clone/actions/workflows/deploy.yml/badge.svg)
 
 This is a modernized fork of <https://github.com/CelesteComet/BabylonJS-Doom-Clone> that adds TypeScript and Vite support for faster development.
+
+View the live site here: <https://symbitic.github.io/BabylonJS-Doom-Clone/>
+
+To run locally, clone the repository and run this:
+
+    cd BabylonJS-Doom-Clone
+    npm install
+    npm run dev
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
-import {
-    Material,
-    StandardMaterial,
-    Texture,
-} from "@babylonjs/core";
-import { scene, assetsManager } from './globals';
+import { Material, StandardMaterial, Texture } from "@babylonjs/core";
+import { scene, assetsManager } from "./globals";
 import { options } from "./options";
 import { Game } from "./game";
 
-const { verbose } = options;
+const { debug, verbose } = options;
+
+if (import.meta.env.DEV || debug) {
+    await import("@babylonjs/inspector");
+    // Hide/show the Inspector
+    window.addEventListener("keydown", (ev) => {
+        // Ctrl+I
+        if (ev.ctrlKey && ev.key === "I") {
+            if (scene.debugLayer.isVisible()) {
+                scene.debugLayer.hide();
+            } else {
+                scene.debugLayer.show();
+            }
+        }
+    });
+}
 
 export interface IAssets {
     materials: Record<string, Material>;
@@ -24,17 +35,17 @@ interface ITexture {
 }
 
 const textures: Record<string, ITexture> = {
-    'wireFrame': { type: 'diffuse', fileName: 'woodenCrate.jpg' }, // change in the future
-    'bulletHoleMaterial': { type: 'diffuse', fileName: 'impact.png' },
-    'woodenCrate': { type: 'diffuse', fileName: 'woodenCrate.jpg' },
-    'e1m1wall': { type: 'diffuse', fileName: 'e1m1wall.png' },
-    //'e1m1wallBump': { type: 'bump', for: 'e1m1wall', fileName: 'NormalMap.png' },
-    'e1m1floor': { type: 'diffuse', fileName: 'e1m1floor.png' },
-    'e1m1floorBump': { type: 'bump', for: 'e1m1floor', fileName: 'bump_e1m1floor.png' },
-    'e1m1ceil': { type: 'diffuse', fileName: 'e1m1ceil.png' },
-    //'e1m1ceilBump': { type: 'bump', for: 'e1m1ceil', fileName: 'bump_e1m1ceil.png' },
-    'test': { type: 'diffuse', fileName: 'test.png' },
-    'testBump': { type: 'bump', for: 'test', fileName: 'bump_test.png' }
+    "wireFrame": { type: "diffuse", fileName: "woodenCrate.jpg" }, // change in the future
+    "bulletHoleMaterial": { type: "diffuse", fileName: "impact.png" },
+    "woodenCrate": { type: "diffuse", fileName: "woodenCrate.jpg" },
+    "e1m1wall": { type: "diffuse", fileName: "e1m1wall.png" },
+    //"e1m1wallBump": { type: "bump", for: "e1m1wall", fileName: "NormalMap.png" },
+    "e1m1floor": { type: "diffuse", fileName: "e1m1floor.png" },
+    "e1m1floorBump": { type: "bump", for: "e1m1floor", fileName: "bump_e1m1floor.png" },
+    "e1m1ceil": { type: "diffuse", fileName: "e1m1ceil.png" },
+    //"e1m1ceilBump": { type: "bump", for: "e1m1ceil", fileName: "bump_e1m1ceil.png" },
+    "test": { type: "diffuse", fileName: "test.png" },
+    "testBump": { type: "bump", for: "test", fileName: "bump_test.png" }
 };
 
 const numberOfTextures = Object.keys(textures).length;
@@ -44,17 +55,17 @@ for (let textureName in textures) {
     if (verbose) {
         console.log(`loading ${textureName}`)
     }
-    const imageTask = assetsManager.addImageTask(textureName, 'textures/' + textures[textureName].fileName);
+    const imageTask = assetsManager.addImageTask(textureName, "textures/" + textures[textureName].fileName);
     imageTask.onSuccess = () => {
         if (verbose) {
-            console.log(`loaded ${textureName}`);
+            console.log(`Loaded ${textureName}`);
         }
         loadedTextures++;
         if (textures[textureName].for) {
-            (assets.materials[textures[textureName].for!] as any).bumpTexture = new Texture('textures/' + textures[textureName].fileName);
+            (assets.materials[textures[textureName].for!] as any).bumpTexture = new Texture("textures/" + textures[textureName].fileName);
         } else {
             assets.materials[textureName] = new StandardMaterial(textureName, scene);
-            (assets.materials[textureName] as any)[textures[textureName].type + 'Texture'] = new Texture('textures/' + textures[textureName].fileName);
+            (assets.materials[textureName] as any)[textures[textureName].type + "Texture"] = new Texture("textures/" + textures[textureName].fileName);
         }
         if (loadedTextures == numberOfTextures) {
             (assets.materials.bulletHoleMaterial as any).diffuseTexture.hasAlpha = true;
@@ -70,9 +81,6 @@ for (let textureName in textures) {
 
             (assets.materials.test as any).diffuseTexture.hasAlpha = true;
             assets.materials.test.zOffset = -2;
-
-
-
         }
     }
 };
@@ -85,11 +93,11 @@ assetsManager.onFinish = () => {
 assetsManager.load();
 
 /*
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
     console.log("Loaded");
     console.log(typeof canvas);
     console.log(typeof assetsManager)
-    //let canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
+    //let canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
     //let app = new App(canvas);
     //app.run();
 });
